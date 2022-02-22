374253d3ff803513b996588957cdaa3a330edd5a
Tomilov Sergey <tomexpert@yandex.ru>
2022-02-22 14:51:21 +0800

    test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/All/i);
+  const linkElement = screen.getByText(/Account Settings/i);
   expect(linkElement).toBeInTheDocument();
 });
